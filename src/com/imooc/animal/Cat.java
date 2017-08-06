9e7b60ef3ea1ff9a941ee08be8acd7059bcb5e98
gzzwinner <gzzwinner@163.com>
2017-08-06 22:30:20 +0800

    重新提交

--- src/com/imooc/animal/Cat.java
+++ src/com/imooc/animal/Cat.java
@@ -8,10 +8,20 @@ package com.imooc.animal;
 public class Cat {
 
 	//属性：昵称、年龄、体重、品种
-	String name;//昵称 默认值为null
-	int month;//年龄  默认值为 0
-	double weight;//体重 默认值为0.0
-	String speices;//品种 默认值为null
+	//修改属性的可见性--private 限定只在当前类内访问
+	private String name;//昵称 默认值为null
+	private int month;//年龄  默认值为 0
+	private double weight;//体重 默认值为0.0
+	private String speices;//品种 默认值为null
+	
+	public Cat(){
+		//System.out.println("不带参数的构造方法");
+	}
+	
+	public Cat(int month){
+		//this.month = month;
+		this.setMonth(month);
+	}
 	
 	public Cat(String name, int month, double weight, String speices){
 		this.name = name;
@@ -19,10 +29,45 @@ public class Cat {
 		this.weight = weight;
 		this.speices = speices;
 	}
-	
-	public Cat(){
-		System.out.println("不带参数的构造方法");
+	//创建get/set方法
+	//get/set方法中添加对属性的限定
+	public void setName(String name){
+		this.name = name;
 	}
+	
+	public String getName(){
+		return "我是一只叫：" + this.name + "的";
+	}
+	
+	
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		if(month<=0){
+			System.out.println("输入信息错误，宠物猫的年龄必须大于0");
+		}else{
+			this.month = month;
+		}
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public String getSpeices() {
+		return speices;
+	}
+
+	public void setSpeices(String speices) {
+		this.speices = speices;
+	}
+
 	//方法：跑动、吃东西
 	//跑动的方法
 	public void run() {
--- src/com/imooc/animal/CatTest.java
+++ src/com/imooc/animal/CatTest.java
@@ -4,19 +4,22 @@ public class CatTest {
 
 	public static void main(String[] args) {
 		//对象实例化
-		Cat one = new Cat("花花", 4, 1000, "英国短毛猫");
+		Cat one = new Cat(3);
 	
-//		one.name = "花花";
-//		one.month = 4;
+		//one.setName("花花");
+		//one.setMonth(-3);
 //		one.weight = 1000;
 //		one.speices = "英国短毛猫";
 //		
-		System.out.println("年龄:" + one.month);
-		System.out.println("姓名:" + one.name);
-		System.out.println("品种:" + one.speices);
-		System.out.println("体重:" + one.weight);
+		if( one.getMonth()==0){
+			return;
+		}
+		System.out.println("年龄:" + one.getMonth());
+		//System.out.println("姓名:" + one.getName());
+//		System.out.println("品种:" + one.speices);
+//		System.out.println("体重:" + one.weight);
 		
-		one.run();
+		//one.run();
 	}
 
 }
